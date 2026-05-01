1b638464e414e824bdb20240d8ad211d9296c1ad
Bjorn <b.stephany@student.maastrichtuniversity.nl>
2026-05-01 10:03:21 +0200

    edit

---
+++ database/Indexes.sql
@@ -0,0 +1,13 @@
+-- Design at least 2 indexes to optimize the performance of these views and overall query speed. 
+-- You need to document the performance of queries before and after the index application
+-- to demonstrate the improvement and justify the application
+
+-- PRE INDEX VIEW SPEEDS:
+-- SHOW_MOST_ACTIVE_USER = 
+-- LIST_USERS_WITHOUT_POSTS = 
+-- SHOW_POPULAR_POSTS = 
+
+-- POST INDEX VIEW SPEEDS:
+-- SHOW_MOST_ACTIVE_USER = 
+-- LIST_USERS_WITHOUT_POSTS = 
+-- SHOW_POPULAR_POSTS = 
--- database/Views.sql
+++ database/Views.sql
@@ -16,17 +16,3 @@ CREATE VIEW SHOW_POPULAR_POSTS AS
 SELECT username, post_id, image_path, caption, COUNT(Like.username) AS like_count from Post
 JOIN Like on `Like`.post_id = Post.post_id
 GROUP BY `Like`.post_id, username, post_id, image_path, caption HAVING COUNT(Like.username) > 100;
-
--- Design at least 2 indexes to optimize the performance of these views and overall query speed. 
--- You need to document the performance of queries before and after the index application
--- to demonstrate the improvement and justify the application
-
--- PRE INDEX VIEW SPEEDS:
--- SHOW_MOST_ACTIVE_USER = 
--- LIST_USERS_WITHOUT_POSTS = 
--- SHOW_POPULAR_POSTS = 
-
--- POST INDEX VIEW SPEEDS:
--- SHOW_MOST_ACTIVE_USER = 
--- LIST_USERS_WITHOUT_POSTS = 
--- SHOW_POPULAR_POSTS = 
